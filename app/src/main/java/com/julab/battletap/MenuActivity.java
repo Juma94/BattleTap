f0cd76d205fc4b6f5c2439742d2aa5861da3ffc8
abdel-m <abdel.mesnaoui@gmail.com>
2015-10-07 17:18:49 +0200

    MenuActivity cleaning

--- app/src/main/java/com/julab/battletap/MenuActivity.java
+++ app/src/main/java/com/julab/battletap/MenuActivity.java
@@ -3,42 +3,12 @@ package com.julab.battletap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
-import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MenuActivity extends AppCompatActivity
 {
-    private ExpandableListView listSolo, listMultiplayer;
-    private ExpandableListAdapter adapterSolo, adapterMulti;
-    private List<String> soloList, multiList;
-
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
-        //Initialisation List
-        soloList = new ArrayList<String>();
-        soloList.add("Fight the numbers");
-        String[] catSolo = soloList.toArray(new String[soloList.size()]);
-        multiList = new ArrayList<String>();
-        multiList.add("Fight the time");
-        multiList.add("Fight the increment");
-        multiList.add("Fight the time & increment");
-        String[] catMulti = multiList.toArray(new String[multiList.size()]);
-
-        listSolo = (ExpandableListView) findViewById(R.id.expandableListSolo);
-        //adapterSolo.
-        listSolo.setAdapter(adapterSolo);
-
-
-    }*/
-
     private ExpandListAdapter ExpAdapter;
     private ArrayList<ExpandListGroup> ExpListItems;
     private ExpandableListView ExpandList;
@@ -50,15 +20,16 @@ public class MenuActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        // Create all components for ExpandableListView
         ExpandList = (ExpandableListView) findViewById(R.id.expandableListSolo);
         ExpListItems = SetStandardGroups();
         ExpAdapter = new ExpandListAdapter(MenuActivity.this, ExpListItems);
         ExpandList.setAdapter(ExpAdapter);
     }
 
+    // Manage string menu of ExpandableListView
     public ArrayList<ExpandListGroup> SetStandardGroups()
     {
-        // test2
         ArrayList<ExpandListGroup> listGroup = new ArrayList<ExpandListGroup>();
         ArrayList<ExpandListChild> listChild = new ArrayList<ExpandListChild>();
 
