b4173d1f2fe701a9d950ebc8c7ce8e1724318191
abdel-m <abdel.mesnaoui@gmail.com>
2015-10-12 17:59:32 +0200

    adding counter to Gaming board solo

--- app/src/main/java/com/julab/battletap/BoardGameSoloActivity.java
+++ app/src/main/java/com/julab/battletap/BoardGameSoloActivity.java
@@ -6,15 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class BoardGameSoloActivity extends AppCompatActivity
 {
-    private int[] tabNumbers = new int[10];
+    private ArrayList<Integer> tabNumbers;
     private TextView nbCaught, nbToCatch, nbTaps;
     private int current, nbCurrentTaps, nbCurrentCaught;
     private Button btnPush, btnConfirm;
+    private Chronometer chronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,23 +26,27 @@ public class BoardGameSoloActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_game_solo);
         ClickListener listenerObj = new ClickListener();
+
+        int increment = (int) (Math.random() * 101);
+        int multiplier = (int) (Math.random() * 101);
+        int germ = (int) (Math.random() * 101);
+
+        tabNumbers = new ArrayList<Integer>();
+        tabNumbers.add(randomNumber(germ, multiplier, increment));
+
         nbCurrentTaps = 0;
         nbCurrentCaught = 0;
-        updateNbTaps();
-        fillTabNumbers();
         current = 0;
-        int i = 0;
-        while (i < tabNumbers.length)
-        {
-            System.out.println(tabNumbers[i]);
-            i++;
-        }
+
+        updateNbTaps();
         updateNumbersCaught();
         updateNumbersToCatch();
         btnPush = (Button) findViewById(R.id.board_game_solo_btnPush);
         btnPush.setOnTouchListener(listenerObj);
         btnConfirm = (Button) findViewById(R.id.board_game_solo_btnConfirm);
         btnConfirm.setOnTouchListener(listenerObj);
+        chronometer = (Chronometer) findViewById(R.id.board_game_solo_chrono);
+        chronometer.start();
     }
 
     public void fillTabNumbers()
@@ -46,16 +54,12 @@ public class BoardGameSoloActivity extends AppCompatActivity
         int increment = (int) (Math.random() * 101);
         int multiplier = (int) (Math.random() * 101);
         int germ = (int) (Math.random() * 101);
+
         System.out.println("increment = " + increment);
         System.out.println("multiplier = " + multiplier);
         System.out.println("germ = " + germ);
-        tabNumbers[0] = randomNumber(germ, multiplier, increment);
-        int i = 1;
-        while (i < tabNumbers.length)
-        {
-            tabNumbers[i] = randomNumber(tabNumbers[i - 1], multiplier, increment);
-            i++;
-        }
+
+        tabNumbers.add(randomNumber(tabNumbers.get(current-1), multiplier, increment));
     }
 
     public int randomNumber(int xPrevious, int multiplier, int increment)
@@ -66,7 +70,7 @@ public class BoardGameSoloActivity extends AppCompatActivity
     public void updateNumbersToCatch()
     {
         nbToCatch = (TextView) findViewById(R.id.board_game_solo_nb_to_catch_current_id);
-        nbToCatch.setText(tabNumbers[current] + "");
+        nbToCatch.setText(tabNumbers.get(current) + "");
     }
 
     public void updateNumbersCaught()
@@ -103,7 +107,7 @@ public class BoardGameSoloActivity extends AppCompatActivity
                 if(v.equals(btnConfirm))
                 {
                     btnConfirm.setBackgroundResource(R.drawable.btn_valider);
-                    if (nbCurrentCaught < tabNumbers.length - 1)
+                    if (tabNumbers.get(current) > nbCurrentCaught)
                     {
                         current++;
                         updateNumbersToCatch();
@@ -111,6 +115,7 @@ public class BoardGameSoloActivity extends AppCompatActivity
                         updateNumbersCaught();
                         nbCurrentTaps = 0;
                         updateNbTaps();
+                        fillTabNumbers();
                     }
                     else
                     {
@@ -123,7 +128,7 @@ public class BoardGameSoloActivity extends AppCompatActivity
                 else if(v.equals(btnPush))
                 {
                     btnPush.setBackgroundResource(R.drawable.push_button);
-                    nbCurrentTaps++;
+                    nbCurrentTaps += (int) (1 + Math.random() * 5); // incrementation aleatoire
                     nbTaps.setText(nbCurrentTaps + "");
                 }
             }
