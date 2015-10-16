94f2120f110d758eead26e7fe1957a7aba7f2758
Juma94 <julien.vdb001@gmail.com>
2015-10-16 20:03:31 +0200

    ActionBar Modify

--- app/src/main/java/com/julab/battletap/FinalizeCreateAccountActivity.java
+++ app/src/main/java/com/julab/battletap/FinalizeCreateAccountActivity.java
@@ -2,12 +2,20 @@ package com.julab.battletap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class FinalizeCreateAccountActivity extends AppCompatActivity {
-
+    private Spinner secretQSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finalize_create_account);
+        secretQSpinner = (Spinner)findViewById(R.id.activity_finalize_create_account_secret_question_id);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Question, android.R.layout.simple_spinner_item);
+        secretQSpinner.setAdapter(adapter);
+        setTitle("Bonjour");
     }
 }
