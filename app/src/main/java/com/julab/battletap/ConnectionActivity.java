49402244ddf1f0290d0e92c51669fb3e64619535
Juma94 <julien.vdb001@gmail.com>
2015-10-17 16:51:10 +0200

    ActionBar Modify + bouton compte

--- app/src/main/java/com/julab/battletap/ConnectionActivity.java
+++ app/src/main/java/com/julab/battletap/ConnectionActivity.java
@@ -1,11 +1,14 @@
 package com.julab.battletap;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConnectionActivity extends AppCompatActivity
 {
@@ -16,7 +19,6 @@ public class ConnectionActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_connection);
         btnConnection = (Button)findViewById(R.id.connectionButton);
         btnConnection.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +42,28 @@ public class ConnectionActivity extends AppCompatActivity
                 startActivity(new Intent(ConnectionActivity.this,PasswordForgottenActivity.class));
             }
         });
+    }
+    @Override
+    public void onBackPressed() {
+        // Dialog to say to user the game are finished
+        AlertDialog.Builder dialog = new AlertDialog.Builder(ConnectionActivity.this);
+        dialog.setMessage(getString(R.string.want_to_quit));
+        dialog.setCancelable(false);
+        dialog.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int id)
+            {
+                ConnectionActivity.this.finish();
+            }
+        });
+        dialog.setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int id)
+            {
+                Toast.makeText(ConnectionActivity.this, R.string.we_miss_you_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.show();
     }
 
 }
--- app/src/main/java/com/julab/battletap/FinalizeCreateAccountActivity.java
+++ app/src/main/java/com/julab/battletap/FinalizeCreateAccountActivity.java
@@ -1,13 +1,17 @@
 package com.julab.battletap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
 public class FinalizeCreateAccountActivity extends AppCompatActivity {
     private Spinner secretQSpinner;
+    private Button btnFinalize;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,13 @@ public class FinalizeCreateAccountActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Question, android.R.layout.simple_spinner_item);
         secretQSpinner.setAdapter(adapter);
+        btnFinalize = (Button)findViewById(R.id.activity_finalize_create_account_btnFinalize_id);
+        btnFinalize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FinalizeCreateAccountActivity.this, ConnectionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
