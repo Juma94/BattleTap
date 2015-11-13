6235288b24b28095874f63f03f0a3570758322ae
Juma94 <julien.vdb001@gmail.com>
2015-11-13 16:59:04 +0100

    score solo fait3

--- app/src/main/java/com/julab/battletap/MyFragment.java
+++ app/src/main/java/com/julab/battletap/MyFragment.java
@@ -5,12 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by user on 25-10-15.
  */
 public class MyFragment extends Fragment {
+    private ListView scoreLst;
+
     //CONSTRUCTOR
+
     public MyFragment()
     {
 
@@ -20,7 +24,8 @@ public class MyFragment extends Fragment {
                              Bundle savedInstanceState)
     {
         View rootView = inflater.inflate(R.layout.activity_score_solo_fragment, container, false);
-
+        scoreLst = (ListView)rootView.findViewById(R.id.score_ranking_fragment_listView);
+        scoreLst.setAdapter(new ScoreRankingListAdapter(this.getContext(), R.layout.expand_list_perso_scores));
 
         return rootView;
 
